8c5b1fa1444a589a1e8bbee7acc49e76a34bba3f
Jake Felice <jasonfelice0@gmail.com>
2022-07-31 03:39:27 -0700

    Fix linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,21 +1,21 @@
 module MyEnumerable
-    def all?(&block)
-       each do |element|
-        return false unless block.call(element)
-       end
-       true
+  def all?(&block)
+    each do |element|
+      return false unless block.call(element)
     end
+    true
+  end
 
-    def any?(&block)
-        each do |element|
-            return true if block.call(element)
-        end
-        false
+  def any?(&block)
+    each do |element|
+      return true if block.call(element)
     end
+    false
+  end
 
-    def filter(&block)
-        newList = []
-        each { |element| newList << element if block.call(element)}
-        newList
-    end
+  def filter(&block)
+    new_list = []
+    each { |element| new_list << element if block.call(element) }
+    new_list
+  end
 end
--- my_list.rb
+++ my_list.rb
@@ -2,12 +2,12 @@ require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
-  
+
   def initialize(*list)
     @list = list
   end
 
   def each
-    @list.each { |e| yield(e) if block_given?}
+    @list.each { |e| yield(e) if block_given? }
   end
 end
